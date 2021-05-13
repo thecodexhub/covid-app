72f11e6240633a6789bf8c645d207f5831aab85d
Sandip <sandipramanik07@gmail.com>
2021-05-13 12:09:17 +0530

    Update failure, Add exceptions and network info

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -1,5 +1,14 @@
 import 'package:equatable/equatable.dart';
 
-abstract class Failure extends Equatable {
-  Failure([List<Object> properties = const <Object>[]]) : super();
+abstract class Failure extends Equatable {}
+
+// General Failures
+class ServerFailure extends Failure {
+  @override
+  List<Object> get props => [];
+}
+
+class CacheFailure extends Failure {
+  @override
+  List<Object> get props => [];
 }
