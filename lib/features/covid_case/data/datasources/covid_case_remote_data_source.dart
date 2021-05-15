cfdca72efd1cc33d80f235773b045be3e70eab98
Sandip <sandipramanik07@gmail.com>
2021-05-15 20:17:58 +0530

    Implement Data layer following TDD

--- lib/features/covid_case/data/datasources/covid_case_remote_data_source.dart
+++ lib/features/covid_case/data/datasources/covid_case_remote_data_source.dart
@@ -1,15 +1,52 @@
+import 'dart:convert';
+
+import 'package:covidapp/core/error/exception.dart';
+import 'package:flutter/foundation.dart';
+
 import '../models/covid_case_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class CovidCaseRemoteDataSource {
   /// Calls the https://api.covid19api.com/{endpoint} api with `summary` endpoint.
   /// Parse the value into `CovidCaseModel` from the Map having the key `Global`.
-  /// 
+  ///
   /// Throws a [ServerException] for all error codes.
   Future<CovidCaseModel> getGlobalCovidCase();
 
   /// Calls the https://api.covid19api.com/{endpoint} api with `summary` endpoint.
   /// Parse the value into List of type `CovidCaseModel` from the Map having the key `Countries`.
-  /// 
+  ///
   /// Throws a [ServerException] for all error codes.
   Future<List<CovidCaseModel>> getCountryCovidCase();
+}
+
+class CovidCaseRemoteDataSourceImpl implements CovidCaseRemoteDataSource {
+  CovidCaseRemoteDataSourceImpl({@required this.client});
+  final http.Client client;
+
+  final String uri = 'https://api.covid19api.com/summary';
+
+  @override
+  Future<CovidCaseModel> getGlobalCovidCase() async {
+    final response = await client.get(Uri.parse(uri));
+    if (response.statusCode == 200) {
+      return CovidCaseModel.fromJson(
+          json.decode(response.body)["Global"] as Map<String, dynamic>);
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<CovidCaseModel>> getCountryCovidCase() async {
+    final response = await client.get(Uri.parse(uri));
+
+    if (response.statusCode == 200) {
+      return List<CovidCaseModel>.from(
+          (json.decode(response.body)["Countries"] as List)
+              .map((e) => CovidCaseModel.fromJson(e as Map<String, dynamic>)));
+    } else {
+      throw ServerException();
+    }
+  }
 }
---
+++ test/features/covid_case/data/datasources/covid_case_remote_data_source_test.dart
@@ -0,0 +1,105 @@
+import 'dart:convert';
+
+import 'package:covidapp/core/error/exception.dart';
+import 'package:covidapp/features/covid_case/data/datasources/covid_case_remote_data_source.dart';
+import 'package:covidapp/features/covid_case/data/models/covid_case_model.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:matcher/matcher.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../../fixtures/fixture.dart';
+
+class MockHttpClient extends Mock implements http.Client {}
+
+void main() {
+  CovidCaseRemoteDataSourceImpl remoteDataSourceImpl;
+  MockHttpClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    remoteDataSourceImpl =
+        CovidCaseRemoteDataSourceImpl(client: mockHttpClient);
+  });
+
+  void setUpMockHttpClientSuccess200(String fixtureString) {
+    when(mockHttpClient.get(any))
+        .thenAnswer((_) async => http.Response(fixture(fixtureString), 200));
+  }
+
+  void setUpMockHttpClientFailure404() {
+    when(mockHttpClient.get(any))
+        .thenAnswer((_) async => http.Response('Something went wrong', 404));
+  }
+
+  group('getGlobalCovidCase', () {
+    final tCovidCaseModel = CovidCaseModel.fromJson(
+        json.decode(fixture('fixture_api_data.json'))["Global"]
+            as Map<String, dynamic>);
+    test('should perform a GET request on a URL with the `summary` endpoint',
+        () async {
+      // Arrange
+      setUpMockHttpClientSuccess200('fixture_api_data.json');
+      // Act
+      await remoteDataSourceImpl.getGlobalCovidCase();
+      // Assert
+      verify(
+          mockHttpClient.get(Uri.parse('https://api.covid19api.com/summary')));
+    });
+
+    test('should return a CovidCaseModel when the statusCode is 200', () async {
+      // Arrange
+      setUpMockHttpClientSuccess200('fixture_api_data.json');
+      // Act
+      final result = await remoteDataSourceImpl.getGlobalCovidCase();
+      // Assert
+      expect(result, equals(tCovidCaseModel));
+    });
+
+    test('should throw ServerException when statusCode is other than 200',
+        () async {
+      // Arrange
+      setUpMockHttpClientFailure404();
+      // Act
+      final call = remoteDataSourceImpl.getGlobalCovidCase;
+      // Assert
+      expect(() => call(), throwsA(TypeMatcher<ServerException>()));
+    });
+  });
+
+  group('getCountryCovidCase', () {
+    final tCovidCaseModelList = List<CovidCaseModel>.from(
+        (json.decode(fixture('fixture_api_data.json'))["Countries"] as List)
+            .map((e) => CovidCaseModel.fromJson(e as Map<String, dynamic>)));
+    test('should perform a GET request on a URL with the `summary` endpoint',
+        () async {
+      // Arrange
+      setUpMockHttpClientSuccess200('fixture_api_data.json');
+      // Act
+      await remoteDataSourceImpl.getCountryCovidCase();
+      // Assert
+      verify(
+          mockHttpClient.get(Uri.parse('https://api.covid19api.com/summary')));
+    });
+
+    test('should return a list of CovidCaseModel when the statusCode is 200',
+        () async {
+      // Arrange
+      setUpMockHttpClientSuccess200('fixture_api_data.json');
+      // Act
+      final result = await remoteDataSourceImpl.getCountryCovidCase();
+      // Assert
+      expect(result, equals(tCovidCaseModelList));
+    });
+
+    test('should throw ServerException when statusCode is other than 200',
+        () async {
+      // Arrange
+      setUpMockHttpClientFailure404();
+      // Act
+      final call = remoteDataSourceImpl.getCountryCovidCase;
+      // Assert
+      expect(() => call(), throwsA(TypeMatcher<ServerException>()));
+    });
+  });
+}
