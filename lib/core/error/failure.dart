f7ae45ea06449af472696f6d28f8d6f0a314e900
Sandip <sandipramanik07@gmail.com>
2021-05-17 00:38:17 +0530

    Add error strings and extensions

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -12,3 +12,21 @@ class CacheFailure extends Failure {
   @override
   List<Object> get props => [];
 }
+
+const String SERVER_FAILURE_MESSAGE =
+    'Server Failed. Please try again after some time.';
+const String CACHE_FAILURE_MESSAGE =
+    'Failed to show data from Cache. Please turn on your wifi.';
+
+extension FailureX on Failure {
+  String get mapFailureToMessage {
+    switch (this.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure:
+        return CACHE_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected error. Please try again.';
+    }
+  }
+}
