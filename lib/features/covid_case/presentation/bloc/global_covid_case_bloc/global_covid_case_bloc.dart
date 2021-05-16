ab1754718359e7023498ea6126ee69337993cc3f
Sandip <sandipramanik07@gmail.com>
2021-05-17 00:39:31 +0530

    Refactoring with error strings and extensions

--- lib/features/covid_case/presentation/bloc/global_covid_case_bloc/global_covid_case_bloc.dart
+++ lib/features/covid_case/presentation/bloc/global_covid_case_bloc/global_covid_case_bloc.dart
@@ -1,22 +1,16 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:covidapp/core/error/failure.dart';
-import 'package:covidapp/core/usecases/usecase.dart';
-import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 
+import '../../../../../core/error/failure.dart';
+import '../../../../../core/usecases/usecase.dart';
 import '../../../domain/entities/covid_case.dart';
 import '../../../domain/usecases/get_global_covid_case.dart';
 
 part 'global_covid_case_event.dart';
 part 'global_covid_case_state.dart';
-
-const String SERVER_FAILURE_MESSAGE =
-    'Server Failed. Please try again after some time.';
-const String CACHE_FAILURE_MESSAGE =
-    'Failed to show data from Cache. Please turn on your wifi.';
 
 class GlobalCovidCaseBloc
     extends Bloc<GlobalCovidCaseEvent, GlobalCovidCaseState> {
@@ -40,19 +34,6 @@ class GlobalCovidCaseBloc
         ),
         (covidCase) => GlobalCovidCaseLoaded(covidCase: covidCase),
       );
-    }
-  }
-}
-
-extension FailureX on Failure {
-  String get mapFailureToMessage {
-    switch (this.runtimeType) {
-      case ServerFailure:
-        return SERVER_FAILURE_MESSAGE;
-      case CacheFailure:
-        return CACHE_FAILURE_MESSAGE;
-      default:
-        return 'Unexpected error. Please try again.';
     }
   }
 }
