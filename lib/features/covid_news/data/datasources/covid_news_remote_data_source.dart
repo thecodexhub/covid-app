0b937d94345a268b88682850d997dd5229c18e30
Sandip <sandipramanik07@gmail.com>
2021-05-19 20:52:46 +0530

    Implement covid news remote datasource

--- lib/features/covid_news/data/datasources/covid_news_remote_data_source.dart
+++ lib/features/covid_news/data/datasources/covid_news_remote_data_source.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
+import 'package:covidapp/api_key.dart';
+import 'package:covidapp/core/error/exception.dart';
+import 'package:flutter/foundation.dart';
+
 import '../models/covid_news_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class CovidNewsRemoteDatasource {
   /// Calls the https://newsapi.org/v2/{endpoint} api with `top-headlines` endpoint.
@@ -6,9 +13,42 @@ abstract class CovidNewsRemoteDatasource {
   ///
   /// Throws a [ServerException] for all error codes.
   Future<List<CovidNewsModel>> getGlobalCovidNews();
+
   /// Calls the https://newsapi.org/v2/{endpoint} api with `top-headlines` endpoint.
-  /// Parse the value into `CovidNewsModel`. And the query parameter `q` is `in`.
+  /// Parse the value into `CovidNewsModel`. And the query parameter `country` is `in`.
   ///
   /// Throws a [ServerException] for all error codes.
   Future<List<CovidNewsModel>> getIndiaCovidNews();
+}
+
+const String URI_FOR_GLOBAL_NEWS =
+    'https://newsapi.org/v2/top-headlines?category=health&apiKey=$NEWS_API_KEY&language=en&q=covid';
+const String URI_FOR_INDIA_NEWS =
+    'https://newsapi.org/v2/top-headlines?category=health&apiKey=$NEWS_API_KEY&country=in&language=en&q=covid';
+
+class CovidNewsRemoteDataSourceImpl implements CovidNewsRemoteDatasource {
+  CovidNewsRemoteDataSourceImpl({@required this.client});
+  final http.Client client;
+
+  @override
+  Future<List<CovidNewsModel>> getGlobalCovidNews() async {
+    final response = await client.get(Uri.parse(URI_FOR_GLOBAL_NEWS));
+    if (response.statusCode == 200) {
+      return List<CovidNewsModel>.from((json.decode(response.body) as List).map(
+          (json) => CovidNewsModel.fromJson(json as Map<String, dynamic>)));
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<List<CovidNewsModel>> getIndiaCovidNews() async {
+    final response = await client.get(Uri.parse(URI_FOR_INDIA_NEWS));
+    if (response.statusCode == 200) {
+      return List<CovidNewsModel>.from((json.decode(response.body) as List).map(
+          (json) => CovidNewsModel.fromJson(json as Map<String, dynamic>)));
+    } else {
+      throw ServerException();
+    }
+  }
 }
---
+++ test/features/covid_news/data/datasources/covid_news_remote_data_source_test.dart
@@ -0,0 +1,102 @@
+import 'dart:convert';
+
+import 'package:covidapp/core/error/exception.dart';
+import 'package:covidapp/features/covid_news/data/datasources/covid_news_remote_data_source.dart';
+import 'package:covidapp/features/covid_news/data/models/covid_news_model.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:matcher/matcher.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../../fixtures/fixture.dart';
+
+class MockHttpClient extends Mock implements http.Client {}
+
+void main() {
+  CovidNewsRemoteDataSourceImpl remoteDatasourceImpl;
+  MockHttpClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    remoteDatasourceImpl =
+        CovidNewsRemoteDataSourceImpl(client: mockHttpClient);
+  });
+
+  void setUpMockHttpClientSuccess200() {
+    when(mockHttpClient.get(any)).thenAnswer(
+        (_) async => http.Response(fixture('fixture_api_news.json'), 200));
+  }
+
+  void setUpMockHttpClientFailure404() {
+    when(mockHttpClient.get(any))
+        .thenAnswer((_) async => http.Response('Something went wrong', 404));
+  }
+
+  group('getGlobalCovidNews', () {
+    final tCovidNewsModelList = List<CovidNewsModel>.from(
+        (json.decode(fixture('fixture_api_news.json')) as List)
+            .map((e) => CovidNewsModel.fromJson(e as Map<String, dynamic>)));
+    test('should perform a GET request on a global covid news URL', () async {
+      // Arrange
+      setUpMockHttpClientSuccess200();
+      // Act
+      await remoteDatasourceImpl.getGlobalCovidNews();
+      // Assert
+      verify(mockHttpClient.get(Uri.parse(URI_FOR_GLOBAL_NEWS)));
+    });
+
+    test('should return a list of CovidNewsModel when the statusCode is 200',
+        () async {
+      // Arrange
+      setUpMockHttpClientSuccess200();
+      // Act
+      final result = await remoteDatasourceImpl.getGlobalCovidNews();
+      // Assert
+      expect(result, tCovidNewsModelList);
+    });
+
+    test('should throw ServerException when statusCode is other than 200',
+        () async {
+      // Arrange
+      setUpMockHttpClientFailure404();
+      // Act
+      final call = remoteDatasourceImpl.getGlobalCovidNews;
+      // Assert
+      expect(() => call(), throwsA(TypeMatcher<ServerException>()));
+    });
+  });
+
+  group('getIndiaCovidNews', () {
+    final tCovidNewsModelList = List<CovidNewsModel>.from(
+        (json.decode(fixture('fixture_api_news.json')) as List)
+            .map((e) => CovidNewsModel.fromJson(e as Map<String, dynamic>)));
+    test('should perform a GET request on a india covid news URL', () async {
+      // Arrange
+      setUpMockHttpClientSuccess200();
+      // Act
+      await remoteDatasourceImpl.getIndiaCovidNews();
+      // Assert
+      verify(mockHttpClient.get(Uri.parse(URI_FOR_INDIA_NEWS)));
+    });
+
+    test('should return a list of CovidNewsModel when the statusCode is 200',
+        () async {
+      // Arrange
+      setUpMockHttpClientSuccess200();
+      // Act
+      final result = await remoteDatasourceImpl.getIndiaCovidNews();
+      // Assert
+      expect(result, tCovidNewsModelList);
+    });
+
+    test('should throw ServerException when statusCode is other than 200',
+        () async {
+      // Arrange
+      setUpMockHttpClientFailure404();
+      // Act
+      final call = remoteDatasourceImpl.getIndiaCovidNews;
+      // Assert
+      expect(() => call(), throwsA(TypeMatcher<ServerException>()));
+    });
+  });
+}
